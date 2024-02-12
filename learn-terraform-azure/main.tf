fbf60d61c33d81c40ad57610348683f728c12fd7
venkatsunilm <venkatsunilm@gmail.com>
2024-02-12 18:00:17 +0000

    Adding VM and NIC modules and referring it from the root module

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -45,4 +45,30 @@ module "virtual-nework" {
 
 }
 
+module "network-interface" {
+  source = "./modules/network-interface"
+
+  vmname = var.vmname
+  location = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id = module.virtual-nework.subnet_id
+}
+
+module "virtual-machine" {
+    source = "./modules/virtual-machine"
+    vmname                  = var.vmname
+    location                = var.location
+    resource_group_name     = var.resource_group_name
+    network_interface_ids   = [module.network-interface.nic_id]
+    vm_size                 = var.vm_size
+    os_disk_type            = var.os_disk_type
+    admin_usename           = var.admin_usename
+    admin_password          = var.admin_password
+    image_publisher         = var.image_publisher
+    image_offer             = var.image_offer
+    image_sku               = var.image_sku
+}
+  
+}
+
 
---
+++ learn-terraform-azure/modules/network-interface/outputs.tf
@@ -0,0 +1,4 @@
+output "nic_id"{
+    description = "id of the netwrok interface card"
+    value = azurerm_network_interface.nic.id
+}
