ceab4434495962faf46fb8a2d0908d296e660b5c
venkatsunilm <venkatsunilm@gmail.com>
2024-02-12 12:11:12 +0000

    :bug: fix: variable subnet type issue 2

--- learn-terraform-azure/modules/virtual-network/main.tf
+++ learn-terraform-azure/modules/virtual-network/main.tf
@@ -9,5 +9,5 @@ resource "azurerm_subnet" "subnet" {
   name = var.subnet_name
   resource_group_name = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = var.virtual_network_address_space
+  address_prefixes = var.subnet_address_prefix
 }
