d8b032f3a66c49610f4f4655acb524da4c11b0d8
Venkat Minchala <venkat.minchala@resoniks.com>
2024-09-25 13:22:35 +0300

    Refactor and streamline GitHub Actions and Terraform setup
    - Created separate environments in GitHub secrets.
    - Added environment section to dynamically pick secrets, simplifying the logic and removing conditional if statements.
    - Implemented multi-environment pipeline logic to automatically trigger based on branches.
    - Updated setup, checkout actions to the latest versions.
    - Created reusable Terraform modules for AKS, networking, and storage.
    - Moved all passing values to tfvars for dynamic usage, making dev and prod code identical and reusable.
    - Removed unnecessary variables by using environment variables to concatenate resource names (RG, VNet, subnet, cluster, storage).
    - Resolved SP az login issue (PII error) by clearing cache and logging in with tenant ID.
    - Refactored main.tf files for development and production, now both use the same template, enhancing reusability.
    "

---
+++ environments/development/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.environment}"
+  location = var.location
+}
+
+module "aks" {
+  source              = "../../modules/aks"
+  cluster_name        = "aks-${var.environment}"
+  node_count          = var.node_count
+  environment         = var.environment
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  dns_prefix          = "${var.environment}-aks"
+  # dns_prefix_private_cluster
+}
+
+module "networking" {
+  source              = "../../modules/networking"
+  vnet_name           = "vnet-${var.environment}"
+  subnet_name         = "subnet-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  environment         = var.environment
+  location            = azurerm_resource_group.rg.location
+}
+
+module "storage" {
+  source               = "../../modules/storage"
+  storage_account_name = "sa${var.environment}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  environment          = var.environment
+  location             = azurerm_resource_group.rg.location
+}
