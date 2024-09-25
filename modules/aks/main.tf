a7c8b2531ac5b6fdfa09e37179dc1d48d8ee6a55
venkatsunilm <venkatsunilm@gmail.com>
2024-09-25 13:25:19 +0300

    Refactor and streamline GitHub Actions and Terraform setup (#13)
    - Created separate environments in GitHub secrets.
    - Added environment section to dynamically pick secrets, simplifying the logic and removing conditional if statements.
    - Implemented multi-environment pipeline logic to automatically trigger based on branches.
    - Updated setup, checkout actions to the latest versions.
    - Created reusable Terraform modules for AKS, networking, and storage.
    - Moved all passing values to tfvars for dynamic usage, making dev and prod code identical and reusable.
    - Removed unnecessary variables by using environment variables to concatenate resource names (RG, VNet, subnet, cluster, storage).
    - Resolved SP az login issue (PII error) by clearing cache and logging in with tenant ID.
    - Refactored main.tf files for development and production, now both use the same template, enhancing reusability.
    "
    
    Co-authored-by: Venkat Minchala <venkat.minchala@resoniks.com>

---
+++ modules/aks/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
+  #   kubernetes_version  = "1.26.3"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_D2_v2"
+    # os_disk_size_gb = 30
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  #   service_principal {
+  #     client_id     = var.appId
+  #     client_secret = var.password
+  #   }
+
+  #   role_based_access_control_enabled = true
+
+  tags = {
+    environment = var.environment
+  }
+
+}
+
---
+++ modules/storage/variables.tf
@@ -0,0 +1,19 @@
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group name"
+  type        = string
+}
+
+variable "environment" {
+  description = "environment"
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region where the AKS cluster will be deployed"
+  type        = string
+}
