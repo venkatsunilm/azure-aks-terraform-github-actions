443c5ca3f6c1c65f43e191251ad1aa93378c5c40
venkatsunilm <venkatsunilm@gmail.com>
2024-09-26 17:35:16 +0300

    Upgrade: Update terraform core and azurerm
    - prevent_deletion_if_contains_resources
    - Used latest azurerm provider with latest terraform core
    - used terraform imports to synch the files from the command line when testing locally with development

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.3.0" # install the latest terraform core if we use version > 4.0
+      version = "=4.0.1" # install the latest terraform core if we use version > 4.0
     }
   }
 }
