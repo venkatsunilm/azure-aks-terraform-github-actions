ee619a4e6db09e1450ce9d46627ac5716d33aac7
venkatsunilm <venkatsunilm@gmail.com>
2024-09-29 00:15:42 +0300

    Refactor: Improve maintainability using locals (#22)
    - Add local sh file to set the local env variables
    - move few reusable variables to locals and use with local
    - On local env, make sure you check docker engine is running otherwise you may 403 authntication issus while creating the storage resource

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,8 +2,8 @@ resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = local.account_tier
+  account_replication_type = local.account_replication_type
 
   tags = {
     environment = var.environment
